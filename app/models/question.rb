05e628fbc045ee71f1e3c16ac8924297735f8ed9
Urubu495 <urubu495@gmail.com>
2024-03-21 15:28:22 +0900

    リレーション設定

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,10 @@
 class Question < ApplicationRecord
+  belongs_to :category
+  belongs_to :question_trend
+  belongs_to :year
+  has_many :choices
+  has_many :session_questions
+  has_many :saved_questions
+  has_many :user_answers
+  has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :sessions
+  has_many :saved_questions
+  has_many :user_answers
+  has_many :comments
 end
