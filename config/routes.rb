03d0d00ea9ef63987c90314ad02594dea8fa17d1
Urubu495 <urubu495@gmail.com>
2024-03-23 15:44:22 +0900

    ヘッダー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'top#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: %i[new create]
+  
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
