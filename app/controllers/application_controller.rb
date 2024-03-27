8e9bc0736ae34d8d303ef8fb84faa414de365e87
Urubu495 <urubu495@gmail.com>
2024-03-27 15:20:07 +0900

    ログイン機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
+  private
+
+  def not_authenticated
+    redirect_to login_path, danger: 'ログインしてください'
+  end
 end
