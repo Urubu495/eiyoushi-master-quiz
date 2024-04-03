15fe51aa0c7ba09bb6f60fb714486df022a2ab5d
Urubu495 <urubu495@gmail.com>
2024-04-03 14:54:45 +0900

    データ追加2(問題、選択肢)

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,9 @@
 # カテゴリーデータ実装済み　load Rails.root.join('db', 'seeds', 'categories.rb')
 # 年度データ実装済み　load Rails.root.join('db', 'seeds', 'years.rb')
 # 頻出傾向データ実装済み　load Rails.root.join('db', 'seeds', 'question_trends.rb')
-load Rails.root.join('db', 'seeds', 'questions_37(1).rb')
+
+#第37回問題
+# 問題データ1実装済み  load Rails.root.join('db', 'seeds', 'questions_37(1).rb') #合計問題数50、選択肢数249
+# 問題データ2実装済み load Rails.root.join('db', 'seeds', 'questions_37(2).rb') #合計問題数100、選択肢数499
+# 問題データ3実装済み load Rails.root.join('db', 'seeds', 'questions_37(3).rb') #合計問題数150、選択肢数746
+# 問題データ4実装済み load Rails.root.join('db', 'seeds', 'questions_37(4).rb') #合計問題数200、選択肢数970
