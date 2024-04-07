e895839e10a150ab02917036ef8b96db9945b97b
Urubu495 <urubu495@gmail.com>
2024-04-07 16:52:28 +0900

    問題出題機能

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,26 @@
+class AnswersController < ApplicationController
+  skip_before_action :require_login, only: %i[create]
+
+def create
+  choice = Choice.find(params[:choice_id])
+  question_ids = session[:questions]
+  current_question_id = choice.question_id
+  current_index = question_ids.index(current_question_id)
+  if current_index && current_index + 1 < question_ids.length
+    @next_question_id = question_ids[current_index + 1]
+  else
+    @next_question_id = nil
+  end
+
+  # 正解の選択肢が何番目かを取得
+  correct_choice = Choice.where(question_id: current_question_id, is_answer: true).first
+  correct_choice_index = Choice.where(question_id: current_question_id).order(:id).index(correct_choice) + 1
+
+  if choice.is_answer
+    render turbo_stream: turbo_stream.replace("answer_button", partial: "correct_answer")
+  else
+    render turbo_stream: turbo_stream.replace("answer_button", partial: "wrong_answer", locals: {next_question_id: @next_question_id, correct_choice_index: correct_choice_index})
+  end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+  
+  get 'questions/setting', to: 'questions#setting', as: 'questions_setting'
+  post 'questions/create', to: 'questions#create', as: 'questions_create'
+  resources :questions, only: [:show]
+  post 'answers', to: 'answers#create', as: 'your_answer'
+
+  post 'questions/:id/save', to: 'questions#save', as: 'your_save_question'
 end
