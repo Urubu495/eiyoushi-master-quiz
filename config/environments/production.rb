698000f7fd041acf87a6a36d0bffbba06919b4ca
Urubu495 <urubu495@gmail.com>
2024-05-11 10:34:55 +0900

    SendGrid Test2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
   :user_name      => 'apikey',
-  :password       => ENV[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],
+  :password       => ENV['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'],
   :domain => 'herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
