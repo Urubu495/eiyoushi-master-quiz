7d6aa18ad5a87db9a7e7ca42a80fd84636725a24
Urubu495 <urubu495@gmail.com>
2024-05-19 13:38:11 +0900

    メール送信機能実装

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,40 +1,50 @@
-require 'sendgrid-ruby'
-include SendGrid
-
 class UserMailer < ApplicationMailer
   def reset_password_email(user)
-    @user = User.find(user.id)
+    @user = user
     @url  = edit_password_reset_url(@user.reset_password_token)
 
-    from = Email.new(email: 'from@example.com')
-    to = Email.new(email: @user.email)
-    subject = I18n.t('defaults.password_reset')
-    content = Content.new(type: 'text/plain', value: render_to_string(template: 'user_mailer/reset_password_email'))
-    mail = Mail.new(from, subject, to, content)
+    from = SendGrid::Email.new(email: 'ha.business123@gmail.com') # 送信元アドレス
+    to = SendGrid::Email.new(email: @user.email) # 送信先アドレス
+    subject = t('defaults.password_reset')
+    
+    # メールの本文をHTMLでレンダリング
+    template = ApplicationController.render(
+      template: 'user_mailer/reset_password_email',
+      layout: false,
+      assigns: { user: @user, url: @url }
+    )
+
+    content = SendGrid::Content.new(type: 'text/html', value: template)
+    mail = SendGrid::Mail.new(from, subject, to, content)
 
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
     response = sg.client.mail._('send').post(request_body: mail.to_json)
-
-    Rails.logger.info "SendGrid Response: #{response.status_code}"
-    Rails.logger.info response.body
-    Rails.logger.info response.headers
   end
+  # 元はメールは1度、送信すると5分間は連続してメールは送れない(sorceryのデフォルト設定)
+  # 5秒間隔で送れるように変更している
 
   def email_change_confirmation(user)
     @user = user
     @confirmation_token = user.confirmation_token
 
-    from = Email.new(email: 'from@example.com')
-    to = Email.new(email: @user.unconfirmed_email)
-    subject = I18n.t('defaults.email_change')
-    content = Content.new(type: 'text/plain', value: render_to_string(template: 'user_mailer/email_change_confirmation'))
-    mail = Mail.new(from, subject, to, content)
+    from = SendGrid::Email.new(email: 'ha.business123@gmail.com') # 送信元アドレス
+    to = SendGrid::Email.new(email: @user.unconfirmed_email) # 送信先アドレス
+    subject = t('defaults.email_change')
+
+    host = Settings.default_url_options.host
+    @url = confirm_email_url(token: @confirmation_token, host: host)
+    
+    # メールの本文をHTMLでレンダリング
+    template = ApplicationController.render(
+      template: 'user_mailer/email_change_confirmation',
+      layout: false,
+      assigns: { user: @user, url: @url }
+    )
+
+    content = SendGrid::Content.new(type: 'text/html', value: template)
+    mail = SendGrid::Mail.new(from, subject, to, content)
 
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
     response = sg.client.mail._('send').post(request_body: mail.to_json)
-
-    Rails.logger.info "SendGrid Response: #{response.status_code}"
-    Rails.logger.info response.body
-    Rails.logger.info response.headers
   end
 end
