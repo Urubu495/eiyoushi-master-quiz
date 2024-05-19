01e4d59fecbd034cfd519e38811d8677683c9c28
Urubu495 <urubu495@gmail.com>
2024-05-19 13:38:11 +0900

    メール送信機能実装

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'https://eiyoushi-master-quiz-1d4150fdbd38.herokuapp.com/' }
+  config.action_mailer.default_url_options = Settings.default_url_options.to_h
 
   config.action_mailer.raise_delivery_errors = true
 
